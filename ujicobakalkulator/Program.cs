e0eae7ea595032f651d9968227ad02164289e46d
Shellyn Euriska <lycorisilytheayeh@gmail.com>
2023-01-08 15:53:51 +0700

    first commit

---
+++ ujicobakalkulator/Program.cs
@@ -0,0 +1,14 @@
+﻿calculator.kalkulator casio = new calculator.kalkulator();
+
+casio.pengurangan();
+casio.penjumlahan();
+casio.perkalian();
+casio.pembagian();
+
+hitungbangundatar.bangundatar bangun = new hitungbangundatar.bangundatar();
+bangun.lpersegi();
+bangun.kpersegi();
+bangun.lpersegipanjang();
+bangun.kpersegipanjang();
+bangun.lsegitiga();
+bangun.ksegitiga();
